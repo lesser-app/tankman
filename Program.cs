f642ce4bb6931c32d14e13d649182e4fb4c34d0f
Jeswin Kumar <jeswinpk@agilehead.com>
2023-07-02 18:46:24 +0530

    Add initdb support

--- Program.cs
+++ Program.cs
@@ -56,6 +56,10 @@ var maxResultsOption = new Option<int>(
             description: "Maximum number of results to return.",
             getDefaultValue: () => 10000);
 
+var initDbOption = new Option<bool>(
+            name: "--initdb",
+            description: "Initialize the database.");
+
 var safetyKeyArg = new Option<string?>(
   name: "--safety-key",
   description: "Require this key to be supplied as the querystring parameter 'safetyKey' for org deletion."
@@ -72,11 +76,14 @@ rootCommand.AddOption(wildcardOption);
 rootCommand.AddOption(separatorOption);
 rootCommand.AddOption(safetyKeyArg);
 rootCommand.AddOption(maxResultsOption);
+rootCommand.AddOption(initDbOption);
 
 string host = Environment.GetEnvironmentVariable("TANKMAN_HOST") ?? "localhost";
 int port = !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("TANKMAN_PORT"))
   ? int.Parse(Environment.GetEnvironmentVariable("TANKMAN_PORT")!)
   : 1989;
+
+bool initDb = false;
 
 rootCommand.SetHandler((context) =>
 {
@@ -87,6 +94,7 @@ rootCommand.SetHandler((context) =>
   {
     host = hostOptionResult.GetValueOrDefault<string>()!;
   }
+
   if (!portOptionResult.IsImplicit)
   {
     port = portOptionResult.GetValueOrDefault<int>();
@@ -101,6 +109,7 @@ rootCommand.SetHandler((context) =>
   var separatorCharacter = context.ParseResult.GetValueForOption(separatorOption);
   var safetyKey = context.ParseResult.GetValueForOption(safetyKeyArg);
   var maxResults = context.ParseResult.GetValueForOption(maxResultsOption);
+  initDb = context.ParseResult.GetValueForOption(initDbOption);
 
   Settings.MaxResults = maxResults;
   Settings.Wildcard = wildcardCharacter!;
@@ -119,86 +128,95 @@ rootCommand.SetHandler((context) =>
 
 rootCommand.Invoke(args);
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.ConfigureHttpJsonOptions(options =>
+if (!initDb)
 {
-  options.SerializerOptions.PropertyNameCaseInsensitive = false;
-  options.SerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
-  options.SerializerOptions.DictionaryKeyPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
-  options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-  options.SerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-});
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+  var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+  builder.Services.ConfigureHttpJsonOptions(options =>
+  {
+    options.SerializerOptions.PropertyNameCaseInsensitive = false;
+    options.SerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+    options.SerializerOptions.DictionaryKeyPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+    options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    options.SerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+  });
 
-var orgsApi = app.MapGroup("orgs");
+  builder.Services.AddEndpointsApiExplorer();
+  builder.Services.AddSwaggerGen();
 
-// ORGS
-orgsApi.MapGet("/{orgId?}", OrgHandlers.GetOrgsAsync).WithOpenApi();
-orgsApi.MapPost("/", OrgHandlers.CreateOrgAsync).WithOpenApi();
-orgsApi.MapPut("/{orgId}", OrgHandlers.UpdateOrgAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}", OrgHandlers.DeleteOrgAsync).WithOpenApi();
-// ORG PROPERTIES
-orgsApi.MapGet("/{orgId}/properties/{name?}", OrgHandlers.GetPropertiesAsync).WithOpenApi();
-orgsApi.MapPut("/{orgId}/properties/{name}", OrgHandlers.UpdatePropertyAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}/properties/{name}", OrgHandlers.DeletePropertyAsync).WithOpenApi();
+  var app = builder.Build();
 
+  var orgsApi = app.MapGroup("orgs");
 
-// RESOURCES
-orgsApi.MapGet("/{orgId}/resources/{*resourceId}", ResourceHandlers.GetResourcesAsync).WithOpenApi();
-orgsApi.MapPost("/{orgId}/resources", ResourceHandlers.CreateResourceAsync).WithOpenApi();
-orgsApi.MapPut("/{orgId}/resources/{*resourceId}", ResourceHandlers.UpdateResourceAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}/resources/{*resourceId}", ResourceHandlers.DeleteResourceAsync).WithOpenApi();
+  // ORGS
+  orgsApi.MapGet("/{orgId?}", OrgHandlers.GetOrgsAsync).WithOpenApi();
+  orgsApi.MapPost("/", OrgHandlers.CreateOrgAsync).WithOpenApi();
+  orgsApi.MapPut("/{orgId}", OrgHandlers.UpdateOrgAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}", OrgHandlers.DeleteOrgAsync).WithOpenApi();
+  // ORG PROPERTIES
+  orgsApi.MapGet("/{orgId}/properties/{name?}", OrgHandlers.GetPropertiesAsync).WithOpenApi();
+  orgsApi.MapPut("/{orgId}/properties/{name}", OrgHandlers.UpdatePropertyAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}/properties/{name}", OrgHandlers.DeletePropertyAsync).WithOpenApi();
 
 
-// USERS
-orgsApi.MapGet("/{orgId}/users/{userId?}", UserHandlers.GetUsersAsync).WithOpenApi();
-orgsApi.MapPost("/{orgId}/users", UserHandlers.CreateUserAsync).WithOpenApi();
-orgsApi.MapPut("/{orgId}/users/{userId}", UserHandlers.UpdateUserAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}/users/{userId}", UserHandlers.DeleteUserAsync).WithOpenApi();
-// USER ROLES
-orgsApi.MapPost("/{orgId}/users/{userId}/roles", UserHandlers.AssignRoleAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}/users/{userId}/roles/{roleId}", UserHandlers.UnassignRoleAsync).WithOpenApi();
-// USER PERMISSIONS
-orgsApi.MapGet("/{orgId}/users/{userId}/permissions/{action?}/{*resourceId}", UserPermissionHandlers.GetUserPermissionsAsync).WithOpenApi();
-orgsApi.MapPost("/{orgId}/users/{userId}/permissions", UserPermissionHandlers.CreateUserPermissionAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}/users/{userId}/permissions/{action}/{*resourceId}", UserPermissionHandlers.DeleteUserPermissionAsync).WithOpenApi();
-// COMBINED USER/ROLE
-orgsApi.MapGet("/{orgId}/users/{userId}/effective-permissions/{action?}/{*resourceId}", UserPermissionHandlers.GetEffectivePermissionsAsync).WithOpenApi();
-// ROLE PROPERTIES
-orgsApi.MapGet("/{orgId}/users/{userId}/properties/{name?}", UserHandlers.GetPropertiesAsync).WithOpenApi();
-orgsApi.MapPut("/{orgId}/users/{userId}/properties/{name}", UserHandlers.UpdatePropertyAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}/users/{userId}/properties/{name}", UserHandlers.DeletePropertyAsync).WithOpenApi();
+  // RESOURCES
+  orgsApi.MapGet("/{orgId}/resources/{*resourceId}", ResourceHandlers.GetResourcesAsync).WithOpenApi();
+  orgsApi.MapPost("/{orgId}/resources", ResourceHandlers.CreateResourceAsync).WithOpenApi();
+  orgsApi.MapPut("/{orgId}/resources/{*resourceId}", ResourceHandlers.UpdateResourceAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}/resources/{*resourceId}", ResourceHandlers.DeleteResourceAsync).WithOpenApi();
 
 
-// ROLES
-orgsApi.MapGet("/{orgId}/roles/{roleId?}", RoleHandlers.GetRolesAsync).WithOpenApi();
-orgsApi.MapPost("/{orgId}/roles", RoleHandlers.CreateRoleAsync).WithOpenApi();
-orgsApi.MapPut("/{orgId}/roles/{roleId}", RoleHandlers.UpdateRoleAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}/roles/{roleId}", RoleHandlers.DeleteRoleAsync).WithOpenApi();
-// ROLE USERS
-orgsApi.MapGet("/{orgId}/roles/{roleId}/users", RoleHandlers.GetRoleUsersAsync).WithOpenApi();
-// ROLE PERMISSIONS
-orgsApi.MapGet("/{orgId}/roles/{roleId}/permissions/{action?}/{*resourceId}", RolePermissionHandlers.GetRolePermissionsAsync).WithOpenApi();
-orgsApi.MapPost("/{orgId}/roles/{roleId}/permissions", RolePermissionHandlers.CreateRolePermissionAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}/roles/{roleId}/permissions/{action}/{*resourceId}", RolePermissionHandlers.DeleteRolePermissionAsync).WithOpenApi();
-// ROLE PROPERTIES
-orgsApi.MapGet("/{orgId}/roles/{roleId}/properties/{name?}", RoleHandlers.GetPropertiesAsync).WithOpenApi();
-orgsApi.MapPut("/{orgId}/roles/{roleId}/properties/{name}", RoleHandlers.UpdatePropertyAsync).WithOpenApi();
-orgsApi.MapDelete("/{orgId}/roles/{roleId}/properties/{name}", RoleHandlers.DeletePropertyAsync).WithOpenApi();
+  // USERS
+  orgsApi.MapGet("/{orgId}/users/{userId?}", UserHandlers.GetUsersAsync).WithOpenApi();
+  orgsApi.MapPost("/{orgId}/users", UserHandlers.CreateUserAsync).WithOpenApi();
+  orgsApi.MapPut("/{orgId}/users/{userId}", UserHandlers.UpdateUserAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}/users/{userId}", UserHandlers.DeleteUserAsync).WithOpenApi();
+  // USER ROLES
+  orgsApi.MapPost("/{orgId}/users/{userId}/roles", UserHandlers.AssignRoleAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}/users/{userId}/roles/{roleId}", UserHandlers.UnassignRoleAsync).WithOpenApi();
+  // USER PERMISSIONS
+  orgsApi.MapGet("/{orgId}/users/{userId}/permissions/{action?}/{*resourceId}", UserPermissionHandlers.GetUserPermissionsAsync).WithOpenApi();
+  orgsApi.MapPost("/{orgId}/users/{userId}/permissions", UserPermissionHandlers.CreateUserPermissionAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}/users/{userId}/permissions/{action}/{*resourceId}", UserPermissionHandlers.DeleteUserPermissionAsync).WithOpenApi();
+  // COMBINED USER/ROLE
+  orgsApi.MapGet("/{orgId}/users/{userId}/effective-permissions/{action?}/{*resourceId}", UserPermissionHandlers.GetEffectivePermissionsAsync).WithOpenApi();
+  // ROLE PROPERTIES
+  orgsApi.MapGet("/{orgId}/users/{userId}/properties/{name?}", UserHandlers.GetPropertiesAsync).WithOpenApi();
+  orgsApi.MapPut("/{orgId}/users/{userId}/properties/{name}", UserHandlers.UpdatePropertyAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}/users/{userId}/properties/{name}", UserHandlers.DeletePropertyAsync).WithOpenApi();
 
-var rolesApi = app.MapGroup("roles");
-var resourcesApi = app.MapGroup("resources");
 
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
+  // ROLES
+  orgsApi.MapGet("/{orgId}/roles/{roleId?}", RoleHandlers.GetRolesAsync).WithOpenApi();
+  orgsApi.MapPost("/{orgId}/roles", RoleHandlers.CreateRoleAsync).WithOpenApi();
+  orgsApi.MapPut("/{orgId}/roles/{roleId}", RoleHandlers.UpdateRoleAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}/roles/{roleId}", RoleHandlers.DeleteRoleAsync).WithOpenApi();
+  // ROLE USERS
+  orgsApi.MapGet("/{orgId}/roles/{roleId}/users", RoleHandlers.GetRoleUsersAsync).WithOpenApi();
+  // ROLE PERMISSIONS
+  orgsApi.MapGet("/{orgId}/roles/{roleId}/permissions/{action?}/{*resourceId}", RolePermissionHandlers.GetRolePermissionsAsync).WithOpenApi();
+  orgsApi.MapPost("/{orgId}/roles/{roleId}/permissions", RolePermissionHandlers.CreateRolePermissionAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}/roles/{roleId}/permissions/{action}/{*resourceId}", RolePermissionHandlers.DeleteRolePermissionAsync).WithOpenApi();
+  // ROLE PROPERTIES
+  orgsApi.MapGet("/{orgId}/roles/{roleId}/properties/{name?}", RoleHandlers.GetPropertiesAsync).WithOpenApi();
+  orgsApi.MapPut("/{orgId}/roles/{roleId}/properties/{name}", RoleHandlers.UpdatePropertyAsync).WithOpenApi();
+  orgsApi.MapDelete("/{orgId}/roles/{roleId}/properties/{name}", RoleHandlers.DeletePropertyAsync).WithOpenApi();
+
+  var rolesApi = app.MapGroup("roles");
+  var resourcesApi = app.MapGroup("resources");
+
+  if (app.Environment.IsDevelopment())
+  {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+  }
+
+  app.Run($"http://{host}:{port}");
+
 }
-
-app.Run($"http://{host}:{port}");
-
+// initDb = true. Initialize the database.
+else
+{
+  InitializeDb.Init();
+}
